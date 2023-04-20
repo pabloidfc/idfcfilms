e452c30ae35ef85a0ff3ed4d8ff5eb21a8fa3de3
Pablo Rodriguez <pablordzinfo@gmail.com>
2023-04-20 17:40:04 -0300

    basic structure created

--- src/init.js
+++ src/init.js
@@ -1,3 +1,5 @@
 const headerSubtitle = document.querySelector(".header__subtitle");
 const headerSearchBar = document.querySelector(".search-bar-section__search-bar");
-const trendingTitle = document.querySelector(".trending__title");
+const trendingTitle = document.querySelector(".trending-section__title");
+const trendingSeeMoreBtn = document.querySelector(".trending-section__btn");
+const categoriesSectionTitle = document.querySelector(".categories-section__title");
--- src/language.js
+++ src/language.js
@@ -1,21 +1,29 @@
 let headerSubtitleText = "find your favorite movies here!";
 let headerSearchBarPlaceholderText = "Avengers...";
 let trendingTitleText = "Trending movies";
+let trendingSeeMoreBtnText = "See more";
+let categoriesSectionTitleText = "Categories";
 
 function loadTextsInSpanish() {
     headerSubtitleText = "encuentra tus películas favoritas aquí!"
     headerSearchBarPlaceholderText = "Vengadores..."
     trendingTitleText = "Películas en tendencia";
+    trendingSeeMoreBtnText = "Ver más";
+    categoriesSectionTitleText = "Categorias";
 }
 
 function loadTextsInEnglish() {
     headerSubtitleText = "find your favorite movies here!";
     headerSearchBarPlaceholderText = "Avengers..."
     trendingTitleText = "Trending movies";
+    trendingSeeMoreBtnText = "See more";
+    categoriesSectionTitleText = "Categories";
 }
 
 function loadLanguage() {
     headerSubtitle.innerHTML = headerSubtitleText;
     headerSearchBar.placeholder = headerSearchBarPlaceholderText;
     trendingTitle.innerHTML = trendingTitleText;
+    trendingSeeMoreBtn.innerHTML = trendingSeeMoreBtnText;
+    categoriesSectionTitle.innerHTML = categoriesSectionTitleText;
 }
